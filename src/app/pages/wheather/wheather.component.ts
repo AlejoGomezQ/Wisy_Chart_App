ddda44cf1ac2c380e632cc66a645744126f95a39
AlejoGomezQ <alejandro.gomezq97@gmail.com>
2024-07-12 17:53:29 -0500

    Project structure set up

---
+++ src/app/pages/wheather/wheather.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wheather',
+  standalone: true,
+  imports: [],
+  templateUrl: './wheather.component.html',
+  styleUrl: './wheather.component.css'
+})
+export class WheatherComponent {
+
+}
